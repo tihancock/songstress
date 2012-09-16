ea4e832326742ddf8eac263cdf7dbf8ad77033ce
Tom Hancock <thomas.ian.hancock@gmail.com>
2012-09-16 20:22:50 +0100

    Initial revision

---
+++ songstress.py
@@ -0,0 +1,28 @@
+import os, fnmatch, sys, urllib, simplejson, eventful
+from mutagen.easyid3 import EasyID3
+
+matches = []
+for root, dirnames, filenames in os.walk(sys.argv[1]):
+  for filename in fnmatch.filter(filenames, '*.mp3'):
+      matches.append(os.path.join(root, filename))
+
+artists = set()
+
+for f in matches:
+    try:
+        id3 = EasyID3(f)
+        artists.add(str(id3['artist'][0]))
+    except:
+        pass
+
+api = eventful.API('CdDcXHqV4qpNMq2F')
+
+for a in artists:
+    events = api.call('/events/search', q='music', l='London', keywords=a, date='Future')
+    try:
+        if len(events['events']) > 0:
+            print a
+        for event in events['events']['event']:
+            print "%s at %s" % (event['title'], event['venue_name'])
+    except:
+        pass
